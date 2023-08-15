35ea2a00e46c20df965ec66b7b4d6019941ef6e8
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-08-15 18:51:07 -0500

    init commit

--- javascript projects/nextjs-blog/pages/_app.js
+++
@@ -1,6 +0,0 @@
-// top level component, useful to keep state when navigating between pages, or to add global styles
-import '../styles/globals.css'
-
-export default function App({Component, pageProps}) {
-    return <Component {...pageProps}/>
-}
